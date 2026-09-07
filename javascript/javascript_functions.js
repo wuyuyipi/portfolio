71a391ca4d5c24085d09fb30a8f0f62440e460e8
wuyuyipi <73147226+wuyuyipi@users.noreply.github.com>
2026-09-07 13:17:01 -0400

    Delete javascript directory

--- javascript/javascript_functions.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "90%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-  }
